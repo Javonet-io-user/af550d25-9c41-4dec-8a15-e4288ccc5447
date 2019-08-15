2a57da214ebe19ce3986551987f14d94925bc22f
Javonet <support@javonet.com>
2019-08-15 01:53:40 +0000

    init

---
+++ src/Mensaje/ClassMensaje/Mensaje.java
@@ -0,0 +1,62 @@
+package Mensaje.ClassMensaje;
+
+import Common.Activation;
+import static Common.JavonetHelper.Convert;
+import static Common.JavonetHelper.getGetObjectName;
+import static Common.JavonetHelper.getReturnObjectName;
+import static Common.JavonetHelper.ConvertToConcreteInterfaceImplementation;
+import Common.JavonetHelper;
+import Common.MethodTypeAnnotation;
+import com.javonet.Javonet;
+import com.javonet.JavonetException;
+import com.javonet.JavonetFramework;
+import com.javonet.api.NObject;
+import com.javonet.api.NEnum;
+import com.javonet.api.keywords.NRef;
+import com.javonet.api.keywords.NOut;
+import com.javonet.api.NControlContainer;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.Iterator;
+import java.lang.*;
+import Mensaje.ClassMensaje.*;
+
+public class Mensaje {
+  protected NObject javonetHandle;
+
+  public Mensaje() {
+    try {
+      javonetHandle = Javonet.New("Mensaje.ClassMensaje.Mensaje");
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+    }
+  }
+
+  public Mensaje(NObject handle) {
+    this.javonetHandle = handle;
+  }
+
+  public void setJavonetHandle(NObject handle) {
+    this.javonetHandle = handle;
+  }
+  /** Method */
+
+  @MethodTypeAnnotation(type = "Method")
+  public java.lang.String Mensaje() {
+    try {
+      java.lang.String res = javonetHandle.invoke("Mensaje");
+      if (res == null) return "";
+      return (java.lang.String) res;
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+      return "";
+    }
+  }
+
+  static {
+    try {
+      Activation.initializeJavonet();
+    } catch (java.lang.Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
